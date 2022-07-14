4b6c9a3016bedb3307148d342784cf16d58fb950
muktadir hossain <muktadirhossainrabbi@gmail.com>
2022-07-14 17:26:40 +0600

    hello

---
+++ script.js
@@ -0,0 +1,8 @@
+// const baseUrl = 'https://www.superheroapi.com/api.php/1942497392623335/';
+// const urlID = 245;
+
+// fetch(`${baseUrl}${urlID}`)
+// .then(response => response.json())
+// .then(response =>{
+
+// })echo "# suoperHero-app" >> README.md
